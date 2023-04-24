8861e7b631892fc7dca3d2ea7278f4b4f5530dfd
Jaunty <jaunty5899@gmail.com>
2023-04-24 16:16:29 +0530

    skeleton img added

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const switchA = document.querySelector(".switch");
+const menuUlBg = document.querySelector(".menu");
+const imageBg = document.querySelectorAll(".image");
+const switchContain = document.querySelector(".s-contain");
+
+const darkMode = () => {
+  let sColor;
+  menuUlBg.classList.toggle("bright");
+  if (switchContain.style.borderColor == "var(--bright)")
+    sColor = "var(--dark)";
+  else sColor = "var(--bright)";
+  switchContain.style.borderColor = sColor;
+  switchA.style.backgroundColor = sColor;
+  imageBg.forEach((element) => {
+    element.classList.toggle("bright");
+  });
+  document.body.classList.toggle("dark");
+};
 
 switchA.addEventListener("click", () => {
   switchA.classList.toggle("translate-X");
+  darkMode();
 });
