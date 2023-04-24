20819584ccb1e57a4396038568b4c21d6d348ad1
Jaunty <jaunty5899@gmail.com>
2023-04-24 17:20:05 +0530

    color scheme mode added

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ const switchA = document.querySelector(".switch");
 const menuUlBg = document.querySelector(".menu");
 const imageBg = document.querySelectorAll(".image");
 const switchContain = document.querySelector(".s-contain");
+const navBefore = document.querySelector(".navbar");
 
 const darkMode = () => {
   let sColor;
+
   menuUlBg.classList.toggle("bright");
+
   if (switchContain.style.borderColor == "var(--bright)")
     sColor = "var(--dark)";
   else sColor = "var(--bright)";
+
   switchContain.style.borderColor = sColor;
   switchA.style.backgroundColor = sColor;
   imageBg.forEach((element) => {
     element.classList.toggle("bright");
   });
   document.body.classList.toggle("dark");
+  navBefore.style.color = sColor;
 };
 
 switchA.addEventListener("click", () => {
